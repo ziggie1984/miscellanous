eed6155c08c9ea66a8496f8c20f5f0a21c58fe79
ziggie1984 <90319354+ziggie1984@users.noreply.github.com>
2021-09-12 22:41:08 +0200

    Update igniter2_0.js

--- igniter2_0.js
+++ igniter2_0.js
@@ -22,199 +22,198 @@ const {lnd} = await authenticatedLndGrpc({
 });
 
 
-
   //Fill all the channels separating by a comma; beginning with your channel
-  //Standard Channel Format for example: 697722x1529x0
-  const channelids_standardformat = ['699867x1878x0','699868x1530x1','699804x759x1','699872x1648x1','699822x1842x1','699835x1781x1','699804x2310x0','699822x1818x1','699824x914x0',
-                                    '699896x995x1', '699814x2050x1','699763x1732x1']
-
-  
-  //const channelids_normalformat = ['767153452055330816', '765827441025417217','767133660778397696','753717419910889472','767146855051362304']
+    //Standard Channel Format for example: 697722x1529x0
+    const channelids_standardformat = ['699835x1781x1','699804x2310x0','699822x1818x1','699824x914x0','699896x995x1','699814x2050x1','699763x1732x1','699867x1878x0','699868x1530x1','699804x759x1','699872x1648x1','699822x1842x1']
 
 
-  //Set this to false if you are using channelids in the normalformat
-  const channelstandarformat = true
-
-  //The gossip protocol is slow, with this variable your force zerofees of all channels
-  const forcezerofee = false
+    //const channelids_normalformat = ['767153452055330816', '765827441025417217','767133660778397696','753717419910889472','767146855051362304']
 
 
-  //Destination PubKey, this is the public key of the node who ignites the channel
-  const destination = '02ba62f2cf65a10b9867477f266e67ef7e5f34b8b8b128916497bf54e0603a4693'
+    //Set this to false if you are using channelids in the normalformat
+    const channelstandarformat = true
 
-  //amount to Send
+    //The gossip protocol is slow, with this variable your force zerofees of all channels
+    const forcezerofee = false
 
-  const amount_sats = 10
-  
-  
-    //set max fee you are willing to pay Default 100 ppm
 
-  const feerate = 100
+    //Destination PubKey, this is the public key of the node who ignites the channel
+    const destination = '03e7d88bb91d92982a711ef56fef6f95f89c484b866086a7d0c1ef08e5b144758a'
 
-  //or set to a specific value
-  const max_fee_sats =  feerate * Math.pow(10, -6) * amount_sats
+    //amount to Send
 
-  
-  
-  //Final Payment or Just Probing
-  const final_payment = false
+    const amount_sats = 10
+
+
+      //set max fee you are willing to pay Default 100 ppm
+
+    const feerate = 100
+
+    //or set to a specific value
+    const max_fee_sats =  feerate * Math.pow(10, -6) * amount_sats
 
 
 
-//=======================================Do not change anthing which come after this ======================================
-
-  //Get Information of the Channels
-
-  var routing_channels = []
-  var channel = {}
-  var id = ''
-
-  if (channelstandarformat === true) {
-
-    //console.log('Format of Channels is Standard e.g. 697722x1529x0')
-
-    for (let i = 0; i < channelids_standardformat.length; i++) {
-        channel = await getChannel({lnd,id: channelids_standardformat[i]})
-        routing_channels.push(channel)
-    }
-
-    //console.log(routing_channels)
-
-  }
-
-  else {
+    //Final Payment or Just Probing
+    const final_payment = false
 
 
-    //console.log('Format of Channels is Normal Format e.g. 767153452055330816')
 
-    for (let i = 0; i < channelids_normalformat.length; i++) {
-        id = (await chanFormat({number: channelids_normalformat[i]})).channel
-        //console.log(id)
-        channel = await getChannel({lnd,id})
-        //console.log(channel)
-        routing_channels.push(channel)
-        //console.log(util.inspect(channel, {depth: null}));
+  //=======================================Do not change anthing which come after this ======================================
+
+    //Get Information of the Channels
+
+    var routing_channels = []
+    var channel = {}
+    var id = ''
+
+    if (channelstandarformat === true) {
+
+      //console.log('Format of Channels is Standard e.g. 697722x1529x0')
+
+      for (let i = 0; i < channelids_standardformat.length; i++) {
+          channel = await getChannel({lnd,id: channelids_standardformat[i]})
+          routing_channels.push(channel)
+      }
+
+      //console.log(routing_channels)
 
     }
-    //console.log(routing_channels)
 
-    //console.log(util.inspect(routing_channel_normal_format,{depth:null}))
-
+    else {
 
 
+      //console.log('Format of Channels is Normal Format e.g. 767153452055330816')
 
-  }
+      for (let i = 0; i < channelids_normalformat.length; i++) {
+          id = (await chanFormat({number: channelids_normalformat[i]})).channel
+          //console.log(id)
+          channel = await getChannel({lnd,id})
+          //console.log(channel)
+          routing_channels.push(channel)
+          //console.log(util.inspect(channel, {depth: null}));
 
+      }
+      //console.log(routing_channels)
 
-
-  //Force Fees to Zero, if igniting a channel
-  if (forcezerofee === true){
-    console.log('Zerofee is forced')
-    for (let i = 0; i < routing_channels.length; i++) {
-        //onsole.log(channelids_standardformat[i]);
-        routing_channels[i].policies[0].base_fee_mtokens = 0
-        routing_channels[i].policies[0].fee_rate = 0
-        routing_channels[i].policies[1].base_fee_mtokens = 0
-        routing_channels[i].policies[1].fee_rate = 0
-
-    }
-    //console.dir(routing_channels)
-    //console.log(util.inspect(routing_channels, {depth: null}));
-
-  }
+      //console.log(util.inspect(routing_channel_normal_format,{depth:null}))
 
 
-  const height = (await getHeight({lnd})).current_block_height;
-
-  const mtokens = String(amount_sats * 1000);
-  //console.log(mtokens)
-
-
-  const invoice = await createInvoice({lnd,mtokens});
-  console.log('==========================================')
-  console.log('Invoice (do not share your secret!!!!):')
-  console.log(invoice)
-  console.log('==========================================')
-  const payment = invoice.payment
-  const imtokens = invoice.mtokens
-  const id_payment = invoice.id
-
-
-  let channels = []
-
-
-
-    const res = await routeFromChannels({channels: routing_channels ,destination,cltv_delta: 300, height: height ,messages:[], mtokens : imtokens,payment: payment, total_mtokens:imtokens});
-
-
-    const {route} = res;
-
-    console.log('Final Route: ')
-    console.log(route)
-  
-    const max_fee_msats = max_fee_sats * 1000
-
-    if(parseInt(route.fee_mtokens) <= max_fee_msats){
-
-      console.log("Fee are good to go")
-      console.log("Fees in sats: " + route.fee + " <= Fee Target: " + Math.round(max_fee_sats))
-      console.log("Fees in msats: " + route.fee_mtokens + " <= Fee Target: " + max_fee_msats)
 
 
     }
-    else{
-      console.log("Fee Target is too low")
-      console.log("Fees in sats: " + route.fee + " > = Fee Target in sats: " + Math.round(max_fee_sats))
-      console.log("Fees in msats: " + route.fee_mtokens + " >= Fee Target in msats: " + max_fee_msats)
-      process.exit(1)
+
+
+
+    //Force Fees to Zero, if igniting a channel
+    if (forcezerofee === true){
+      console.log('Zerofee is forced')
+      for (let i = 0; i < routing_channels.length; i++) {
+          //onsole.log(channelids_standardformat[i]);
+          routing_channels[i].policies[0].base_fee_mtokens = 0
+          routing_channels[i].policies[0].fee_rate = 0
+          routing_channels[i].policies[1].base_fee_mtokens = 0
+          routing_channels[i].policies[1].fee_rate = 0
+
+      }
+      //console.dir(routing_channels)
+      //console.log(util.inspect(routing_channels, {depth: null}));
+
     }
 
+
+    const height = (await getHeight({lnd})).current_block_height;
+
+    const mtokens = String(amount_sats * 1000);
+    //console.log(mtokens)
+
+
+    const invoice = await createInvoice({lnd,mtokens});
     console.log('==========================================')
+    console.log('Invoice (do not share your secret!!!!):')
+    console.log(invoice)
+    console.log('==========================================')
+    const payment = invoice.payment
+    const imtokens = invoice.mtokens
+    const id_payment = invoice.id
+
+
+    let channels = []
 
 
 
+      const res = await routeFromChannels({channels: routing_channels ,destination,cltv_delta: 300, height: height ,messages:[], mtokens : imtokens,payment: payment, total_mtokens:imtokens});
 
-  if (final_payment === true){
 
-    try{
-      console.log("PayViaRoute")
-      const payment_output = await payViaRoutes({lnd,id: id_payment, routes: [route]});
-      console.log("Success: Payment Preimage:" + payment_output)
+      const {route} = res;
 
-    }
-    catch(err){
-      var obj_str = util.inspect(err[2]);
+      console.log('Final Route: ')
+      console.log(route)
 
-      console.log(err)
-    }
+      const max_fee_msats = max_fee_sats * 1000
 
-  }
-  else {
-    try{
-      const preimage = (await payViaRoutes({lnd, routes: [route]})).secret;
+      if(parseInt(route.fee_mtokens) <= max_fee_msats){
 
-    }
-    catch(err){
-      var obj_str = util.inspect(err[2]);
+        console.log("Fee are good to go")
+        console.log("Fees in sats: " + route.fee + " <= Fee Target: " + Math.round(max_fee_sats))
+        console.log("Fees in msats: " + route.fee_mtokens + " <= Fee Target: " + max_fee_msats)
 
-      //console.log(err[2].failures[0])
-      //console.log(err)
-      if (err[1] === 'UnknownPaymentHash'){
-
-        console.log('A Route exists you can send the payment if you change const final_payment = true')
-      }
-
-      else {
-
-        console.log('An Error ocurred check your input data: ' + err)
 
       }
+      else{
+        console.log("Fee Target is too low")
+        console.log("Fees in sats: " + route.fee + " > = Fee Target in sats: " + Math.round(max_fee_sats))
+        console.log("Fees in msats: " + route.fee_mtokens + " >= Fee Target in msats: " + max_fee_msats)
+        process.exit(1)
+      }
+      console.log('==========================================')
+
+
+
+
+    if (final_payment === true){
+
+      try{
+        console.log("PayViaRoute")
+        const payment_output = await payViaRoutes({lnd,id: id_payment, routes: [route]});
+        console.log("Success: Payment Preimage:" + payment_output)
+
+      }
+      catch(err){
+        var obj_str = util.inspect(err[2]);
+        console.log("Error occured while paying")
+        console.log(err[2].failures)
+      }
+
+    }
+    else {
+      try{
+        const preimage = (await payViaRoutes({lnd, routes: [route]})).secret;
+
+      }
+      catch(err){
+        var obj_str = util.inspect(err[2]);
+
+        //console.log(err[2].failures[0])
+        //console.log(err)
+        if (err[1] === 'UnknownPaymentHash'){
+
+          console.log('A Route exists you can send the payment if you change const final_payment = true')
+        }
+
+        else {
+
+          console.log('An Error ocurred check your input data: ')
+          console.log(err[2].failures)
+
+
+        }
+
+      }
 
     }
 
-  }
 
-
-}
+    }
 
 main()
